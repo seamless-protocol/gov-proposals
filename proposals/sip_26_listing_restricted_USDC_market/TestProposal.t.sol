b6a5cf10377924634a6b61621e8a70a5682e90e4
DuXXuD <duxxud@gmail.com>
2024-08-03 04:11:53 +0200

    added unit tests

--- proposals/sip_26_listing_restricted_USDC_market/TestProposal.t.sol
+++ proposals/sip_26_listing_restricted_USDC_market/TestProposal.t.sol
@@ -4,16 +4,74 @@ import {Test, console} from "forge-std/Test.sol";
 import {Proposal} from "./Proposal.sol";
 import {GovTestHelper} from "../../helpers/GovTestHelper.sol";
 import {IPoolDataProvider} from "@aave/contracts/interfaces/IPoolDataProvider.sol";
+import {IAaveOracle} from "@aave/contracts/interfaces/IAaveOracle.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SeamlessAddressBook} from "../../helpers/SeamlessAddressBook.sol";
 
 contract TestProposal is Test, GovTestHelper {
     Proposal public proposal;
 
+    IPoolDataProvider poolDataProvider = IPoolDataProvider(SeamlessAddressBook.POOL_DATA_PROVIDER);
+    IAaveOracle aaveOracle = IAaveOracle(SeamlessAddressBook.AAVE_ORACLE);
+
     function setUp() public {
       proposal = new Proposal();
     }
 
-    function test_checkMarketListing_afterPassingProposal() public {
+    function test_checkAToken_afterPassingProposal() public {
       _passProposal(proposal);
+
+      (address aTokenAddress,,) = poolDataProvider.getReserveTokensAddresses(SeamlessAddressBook.SEAMLESS_RESERVED_USDC);
+
+      assertNotEq(aTokenAddress, address(0));
+
+      IERC20Metadata aToken = IERC20Metadata(aTokenAddress);
+
+      assertEq(aToken.name(), "Seamless rUSDC");
+      assertEq(aToken.symbol(), "srUSDC");
+      assertEq(aToken.decimals(), 6);
+    }
+
+    function test_checkReserveConfiguration_afterPassingProposal() public {
+      _passProposal(proposal);
+
+      (
+        uint256 decimals,
+        uint256 ltv,
+        uint256 liquidationThreshold,
+        uint256 liquidationBonus,
+        uint256 reserveFactor,
+        bool usageAsCollateralEnabled,
+        bool borrowingEnabled,
+        bool stableBorrowRateEnabled,
+        bool isActive,
+        bool isFrozen
+      ) = poolDataProvider.getReserveConfigurationData(SeamlessAddressBook.SEAMLESS_RESERVED_USDC);
+
+      assertEq(decimals, 6);
+      assertEq(ltv, 75_00);
+      assertEq(liquidationThreshold, 78_00);
+      assertEq(liquidationBonus, 105_00);
+      assertEq(reserveFactor, 0);
+      assertEq(usageAsCollateralEnabled, true);
+      assertEq(borrowingEnabled, false);
+      assertEq(stableBorrowRateEnabled, false);
+      assertEq(isActive, true);
+      assertEq(isFrozen, false);
+
+      (uint256 borrowCap, uint256 supplyCap) 
+        = poolDataProvider.getReserveCaps(SeamlessAddressBook.SEAMLESS_RESERVED_USDC);
+
+      assertEq(borrowCap, 0);
+      assertEq(supplyCap, 40_000_000);
+    }
+
+    function test_checkAssetSource_afterPassingProposal_sameAsUSDCSource() public {
+      _passProposal(proposal);
+
+      assertEq(
+        aaveOracle.getSourceOfAsset(SeamlessAddressBook.SEAMLESS_RESERVED_USDC),
+        aaveOracle.getSourceOfAsset(SeamlessAddressBook.USDC)
+      );
     }
 }
