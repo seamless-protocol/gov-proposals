92db2589c2f483acd354d81182c453b6174b3ddf
DuXXuD <duxxud@gmail.com>
2024-08-05 15:47:18 +0200

    forge fmt

--- proposals/.template/Proposal.sol
+++ proposals/.template/Proposal.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.21;
 
-import {SeamlessGovProposal, SeamlessAddressBook} from "../../helpers/SeamlessGovProposal.sol";
+import {
+    SeamlessGovProposal,
+    SeamlessAddressBook
+} from "../../helpers/SeamlessGovProposal.sol";
 
 contract Proposal is SeamlessGovProposal {
-  constructor() {
-    _makeProposal();
-  }
+    constructor() {
+        _makeProposal();
+    }
 
-  function _makeProposal() internal virtual override {
-
-  }
+    function _makeProposal() internal virtual override { }
 }
--- proposals/.template/TestProposal.t.sol
+++ proposals/.template/TestProposal.t.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.21;
 
-import {Test, console} from "forge-std/Test.sol";
-import {Proposal} from "./Proposal.sol";
+import { Test, console } from "forge-std/Test.sol";
+import { Proposal } from "./Proposal.sol";
 
 contract TestProposal is Test {
     Proposal public proposal;
 
     function setUp() public {
-      proposal = new Proposal();
+        proposal = new Proposal();
     }
 
-    function test() public {}
+    function test() public { }
 }
