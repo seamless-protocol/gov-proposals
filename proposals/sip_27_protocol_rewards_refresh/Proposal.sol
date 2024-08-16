6aab09c1cf754cd9edb29f70296d8f9b47b6cec3
DuXXuD <duxxud@gmail.com>
2024-08-17 00:20:45 +0200

    add proposal actions

--- proposals/sip_27_protocol_rewards_refresh/Proposal.sol
+++ proposals/sip_27_protocol_rewards_refresh/Proposal.sol
@@ -6,10 +6,29 @@ import {
     SeamlessAddressBook
 } from "../../helpers/SeamlessGovProposal.sol";
 
+import { ISeamEmissionManager } from
+    "@seamless-governance/interfaces/ISeamEmissionManager.sol";
+
 contract Proposal is SeamlessGovProposal {
     constructor() {
         _makeProposal();
     }
 
-    function _makeProposal() internal virtual override { }
+    function _makeProposal() internal virtual override {
+        _addAction(
+            SeamlessAddressBook.SEAM_EMISSION_MANAGER_1,
+            abi.encodeWithSelector(
+                ISeamEmissionManager.claim.selector,
+                SeamlessAddressBook.GUARDIAN_MULTISIG
+            )
+        );
+
+        _addAction(
+            SeamlessAddressBook.SEAM_EMISSION_MANAGER_2,
+            abi.encodeWithSelector(
+                ISeamEmissionManager.claim.selector,
+                SeamlessAddressBook.GUARDIAN_MULTISIG
+            )
+        );
+    }
 }
