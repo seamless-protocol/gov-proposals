ac04a83354502ff7039a02d4eb3a4f31560738a3
fredwes <6827305+fredwes@users.noreply.github.com>
2024-10-14 07:56:01 -0400

    feat: sip 30 rewards refresh 5 (#18)

---
+++ proposals/sip_30_rewards_refresh_5/Proposal.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {
+    SeamlessGovProposal,
+    SeamlessAddressBook
+} from "../../helpers/SeamlessGovProposal.sol";
+import { ISeamEmissionManager } from
+    "@seamless-governance/interfaces/ISeamEmissionManager.sol";
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+contract Proposal is SeamlessGovProposal {
+    uint256 public constant budgetAmount = 500_000 * 1e18;
+
+    constructor() {
+        _makeProposal();
+    }
+
+    function _makeProposal() internal virtual override {
+        _addAction(
+            SeamlessAddressBook.SEAM_EMISSION_MANAGER_2,
+            abi.encodeWithSelector(
+                ISeamEmissionManager.claim.selector,
+                SeamlessAddressBook.TIMELOCK_SHORT
+            )
+        );
+
+        _addAction(
+            SeamlessAddressBook.SEAM,
+            abi.encodeWithSelector(
+                IERC20.transfer.selector,
+                SeamlessAddressBook.GUARDIAN_MULTISIG,
+                budgetAmount
+            )
+        );
+    }
+}
