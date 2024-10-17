4f752b788a8be0b21e0d4357a35f1b24585bd4fb
DuXXuD <duxxud@gmail.com>
2024-10-17 17:45:12 +0200

    moving deploying interest rate to separate script

--- proposals/.template/Proposal.sol
+++ proposals/.template/Proposal.sol
@@ -11,5 +11,7 @@ contract Proposal is SeamlessGovProposal {
         _makeProposal();
     }
 
+    /// @dev This contract is not deployed onchain, do not make transactions to other contracts
+    /// or deploy a contract. Only the view/pure functions of deployed contracts can be called.
     function _makeProposal() internal virtual override { }
 }
