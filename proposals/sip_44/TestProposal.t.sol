96722f644fce90e5cfa1a06237ad982449d78860
fredwes <6827305+fredwes@users.noreply.github.com>
2025-03-24 17:05:50 -0700

    chore: fmt

--- proposals/sip_44/TestProposal.t.sol
+++ proposals/sip_44/TestProposal.t.sol
@@ -31,10 +31,14 @@ contract TestProposal is GovTestHelper {
             seam.balanceOf(SeamlessAddressBook.SEAM_EMISSION_MANAGER_2);
 
         uint256 expectedEmissionClaimAmount1 =
-            _expectedEmissionManagerClaimAmount(ISeamEmissionManager(SeamlessAddressBook.SEAM_EMISSION_MANAGER_1));
+        _expectedEmissionManagerClaimAmount(
+            ISeamEmissionManager(SeamlessAddressBook.SEAM_EMISSION_MANAGER_1)
+        );
 
         uint256 expectedEmissionClaimAmount2 =
-            _expectedEmissionManagerClaimAmount(ISeamEmissionManager(SeamlessAddressBook.SEAM_EMISSION_MANAGER_2));
+        _expectedEmissionManagerClaimAmount(
+            ISeamEmissionManager(SeamlessAddressBook.SEAM_EMISSION_MANAGER_2)
+        );
 
         _passProposalShortGov(proposal);
 
@@ -43,15 +47,17 @@ contract TestProposal is GovTestHelper {
         uint256 timelockBalanceAfter =
             seam.balanceOf(SeamlessAddressBook.TIMELOCK_SHORT);
         uint256 emissionManagerBalanceAfter1 =
-                seam.balanceOf(SeamlessAddressBook.SEAM_EMISSION_MANAGER_1);
+            seam.balanceOf(SeamlessAddressBook.SEAM_EMISSION_MANAGER_1);
         uint256 emissionManagerBalanceAfter2 =
             seam.balanceOf(SeamlessAddressBook.SEAM_EMISSION_MANAGER_2);
 
-        assertEq(guardianBalanceAfter, guardianBalanceBefore + (2_000_000 * 1e18));
+        assertEq(
+            guardianBalanceAfter, guardianBalanceBefore + (2_000_000 * 1e18)
+        );
         assertEq(
             timelockBalanceAfter,
-            timelockBalanceBefore + expectedEmissionClaimAmount1 + expectedEmissionClaimAmount2
-                - (2_000_000 * 1e18)
+            timelockBalanceBefore + expectedEmissionClaimAmount1
+                + expectedEmissionClaimAmount2 - (2_000_000 * 1e18)
         );
         assertEq(
             emissionManagerBalanceBefore1 - emissionManagerBalanceAfter1,
@@ -63,18 +69,21 @@ contract TestProposal is GovTestHelper {
         );
     }
 
-    function _expectedEmissionManagerClaimAmount(ISeamEmissionManager emissionManager) internal returns (uint256) {
+    function _expectedEmissionManagerClaimAmount(
+        ISeamEmissionManager emissionManager
+    ) internal returns (uint256) {
         uint256 snapshotId = vm.snapshot();
 
         _passProposalShortGov(proposal);
 
         uint64 currentTimestamp = uint64(block.timestamp);
 
-        require(vm.revertToAndDelete(snapshotId), "Failed to revert to snapshot");
+        require(
+            vm.revertToAndDelete(snapshotId), "Failed to revert to snapshot"
+        );
 
         uint256 emissionPerSecond = emissionManager.getEmissionPerSecond();
-        uint64 lastClaimedTimestamp =
-            emissionManager.getLastClaimedTimestamp();
+        uint64 lastClaimedTimestamp = emissionManager.getLastClaimedTimestamp();
 
         return (currentTimestamp - lastClaimedTimestamp) * emissionPerSecond;
     }
